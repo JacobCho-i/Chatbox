d6b95cfa3138902216e34e59ed4f802936de1aed
Jacob Choi <junhyungch@gmail.com>
2024-08-09 21:02:07 -0700

    added more styling

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -1,20 +1,45 @@
-export function MessageBubble({ text }) {
-    return (
-      <div style={styles.bubble}>
-        {text}
-      </div>
-    );
+import { colors } from "@mui/material";
+
+export function MessageBubble({ text, id }) {
+    if (id == 0) {
+        return (
+            <div style={styles.myBubble}>
+              {text}
+            </div>
+          );
+    } else {
+        return (
+            <div style={styles.botBubble}>
+              {text}
+            </div>
+          );
+    }
+    
   };
   
   const styles = {
-    bubble: {
-      padding: '10px 15px',
-      margin: '5px 0',
-      borderRadius: '20px',
-      backgroundColor: '#e0e0e0',
-      display: 'inline-block',
-      maxWidth: '80%',
-      wordWrap: 'break-word',
+    myBubble: {
+        padding: '10px 15px',
+        margin: '5px 0',
+        marginRight: 'auto',
+        borderRadius: '20px',
+        backgroundColor: '#33a7ff',
+        display: 'inline-block',
+        maxWidth: '80%',
+        wordWrap: 'break-word',
+        textAlign: 'left',
+        color: '#ffffff'
+    },
+    botBubble: {
+        padding: '10px 15px',
+        margin: '5px 0',
+        marginLeft: 'auto',
+        borderRadius: '20px',
+        backgroundColor: '#e0e0e0',
+        display: 'inline-block',
+        maxWidth: '80%',
+        wordWrap: 'break-word',
+        textAlign: 'left',
     }
 };
   
--- src/components/MessageTableCell.js
+++ src/components/MessageTableCell.js
@@ -1,22 +1,34 @@
+import { Height } from '@mui/icons-material';
 import { MessageBubble } from './MessageBubble';
 
 export function MessageTableCell({ messages }) {
     return (
       <td style={styles.cell}>
-        {messages.map(message => (
-            <div>
-                <MessageBubble key={message.id} text={message.text} />
-            </div>
-        ))}
+        <div style={styles.scrollable}>
+            {messages.map(message => (
+                <div>
+                    <MessageBubble key={message.id} text={message.text} id={0} />
+                </div>
+            ))}
+        </div>
       </td>
     );
   };
   
   const styles = {
     cell: {
+      width: '400px',
       padding: '10px',
       border: '1px solid #ccc',
       verticalAlign: 'top',
+    },
+    scrollable: {
+        height: '400px', 
+        overflowY: 'auto', 
+        display: 'flex',
+        flexDirection: 'column', 
+        alignItems: 'flex-start', 
+        justifyContent: 'flex-start', 
     }
   };
   
